fc7648d6e4b566dda80d6341bde01cd6ca20b1e7
evs-coder <139830264+evs-coder@users.noreply.github.com>
2025-07-20 13:25:45 +0000

    Desafio nivel iniciante.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,9 +6,33 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    
+    //iniciar programa
+
+    printf("\nWELCOME TO SUPER TRUNFO!\n");
+    
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    //informacoes da carta1:
 
+    char estado1;
+    int codigo1;
+    char cidade1[10];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pturisticos1;
+
+    //informacoes da carta2:
+
+    char estado2;
+    int codigo2;
+    char cidade2[10];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pturisticos2;
+    
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -20,6 +44,42 @@ int main() {
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
+
+    //inserir informações da carta1
+
+    printf("\nInsira as informacoes da primeira carta:\n");
+    printf("\nEstado (letra de A-H): ");
+    scanf (" %c", &estado1);
+    printf("Codigo (1-4): ");
+    scanf("%d", &codigo1);
+    printf("Nome da cidade: ");
+    scanf("%s", cidade1);
+    printf("Numero de habitantes: ");
+    scanf("%d", &populacao1);
+    printf("Area: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Quantidade de pontos turisticos: ");
+    scanf("%d", &pturisticos1);
+
+    //inserir informações da carta2
+
+    printf("\nInsira as informacoes da segunda carta:\n");
+    printf("\nEstado (letra de A-H): ");
+    scanf (" %c", &estado2);
+    printf("Codigo (1-4): ");
+    scanf("%d", &codigo2);
+    printf("Nome da cidade: ");
+    scanf("%s", cidade2);
+    printf("Numero de habitantes: ");
+    scanf("%d", &populacao2);
+    printf("Area: ");
+    scanf("%f", &area2);
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    printf("Quantidade de pontos turisticos: ");
+    scanf("%d", &pturisticos2);
 
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
